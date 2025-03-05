cd0eb9f83908ae6da4fdfcc9f2f999074c8405c6
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-05 15:15:36 +0530

    constructor

--- Card.java
+++ Card.java
@@ -1,3 +1,8 @@
 class Card {
     private String rank;
     private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
