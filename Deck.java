37e4214661a0e6994f5cac8915d62ac66d60b6a1
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-05 15:21:31 +0530

    shuffling the deck

--- Deck.java
+++ Deck.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Deck {
     private ArrayList<Card> deck;
 
-// Constructor initializes the deck
+    // Constructor initializes the deck
     public Deck() {
         deck = new ArrayList<>();
         createDeck();
     }
-
     // Creates a deck of 52 cards
     public void createDeck() {
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
@@ -20,17 +19,18 @@ class Deck {
             }
         }
     }
-
-    // Prints a single card
-    public void printCard(Card card) {
-        System.out.println(card);
-    }
-
     // Prints all cards in the deck
     public void printDeck() {
         for (Card card : deck) {
             System.out.println(card);
         }
     }
-
-    
+    // Prints a single card
+    public void printCard(Card card) {
+        System.out.println(card);
+    }
+    // Shuffle the deck
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+        System.out.println("Deck has been shuffled.");
+    }
