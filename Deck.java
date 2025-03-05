a7474ad1828b90dbd35c5b2455c345d220d7b25c
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-05 15:18:24 +0530

    initialize deck with constructor

--- Deck.java
+++ Deck.java
@@ -2,3 +2,9 @@ import java.util.*;
 
 class Deck {
     private ArrayList<Card> deck;
+
+// Constructor initializes the deck
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck();
+    }
